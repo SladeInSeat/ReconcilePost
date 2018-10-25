9fa7a9d086a937aabd144256281b18a2ef6e06b1
slade <sladenator78@gmail.com>
2018-10-25 16:04:01 -0400

    using 2 txt files now, one from arcpy.reconcile and other to aggregate and report. functional for child to qa errors, will stop on errors.

--- basic_reconcile.py
+++ basic_reconcile.py
@@ -2,86 +2,77 @@ from __future__ import print_function
 import arcpy
 import getpass
 import re
-import datetime
+
 
 text_match = '\[(\d+/\d+/\d+.+)]\s(Warning: Conflicts found reconciling version)\s([SDE]+\.\w+)\.'
 
+
 def main():
-    arcpy.env.overwriteOutput = True
-    arcpy.env.workspace,user_name = set_workspace()
-    db_exclude_list = ['SDE@WebAppDev_CLUSTER.sde']
-    conn_files = [file for file in arcpy.ListFiles("SDE@*") if file in db_exclude_list]
-    print (conn_files)
+    try:
+        arcpy.env.overwriteOutput = True
+        arcpy.env.workspace,user_name = set_workspace()
+        db_exclude_list = ['SDE@WebAppDev_CLUSTER.sde']
+        conn_files = [file for file in arcpy.ListFiles("SDE@*") if file in db_exclude_list]
+        print (conn_files)
 
-    for file in conn_files:
-        db_string = r"Database Connections\{}".format(file)
-        versions_children = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string) if version.parentVersionName not in ['sde.DEFAULT',None]]
-        version_QA = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string) if version.parentVersionName == 'sde.DEFAULT' and version.name[-3:]=='_QA']
-        version_default = ['sde.DEFAULT']
-        arcpy.ReconcileVersions_management(file,
-                                           "ALL_VERSIONS",
-                                           version_QA[0],
-                                           versions_children,
-                                           "LOCK_ACQUIRED",
-                                           "ABORT_CONFLICTS",
-                                           "BY_ATTRIBUTE",
-                                           "FAVOR_TARGET_VERSION",
-                                           "POST",
-                                           "KEEP_VERSION",
-                                           r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]))
+        for file in conn_files:
 
-        with open(r"C:\Users\{}\Desktop\actual back up folder\reconcile_log.txt".format(user_name),"a") as out_file,\
-             open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]),'r') as in_fileQA:
-             out_file.write('**************\n')
-             lineQA = in_fileQA.readline()
-             while lineQA:
-                 out_file.write(lineQA)
-                 lineQA = in_fileQA.readline()
-             out_file.write('\n')
+            db_string = r"Database Connections\{}".format(file)
+            reconcile_errors = False
+            versions_children = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string) if version.parentVersionName not in ['sde.DEFAULT',None]]
+            version_QA = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string) if version.parentVersionName == 'sde.DEFAULT' and version.name[-3:]=='_QA']
+            version_default = ['sde.DEFAULT']
+            arcpy.ReconcileVersions_management(file,
+                                               "ALL_VERSIONS",
+                                               version_QA[0],
+                                               versions_children,
+                                               "LOCK_ACQUIRED",
+                                               "ABORT_CONFLICTS",
+                                               "BY_ATTRIBUTE",
+                                               "FAVOR_TARGET_VERSION",
+                                               "POST",
+                                               "KEEP_VERSION",
+                                               r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]))
 
-        with open(r"C:\Users\{}\Desktop\actual back up folder\reconcile_log.txt".format(user_name), "r") as infile, \
-            open(r"C:\Users\{}\Desktop\finalfolder\finalfolderlog.txt".format(user_name), "a") as outfile:
-            line = infile.readline()
-            while line:
-                match = re.search(text_match,line)
-                if match:
-                    print (match.group(1))
-                    print (match.group(2))
-                    print (match.group(3))
-                    outfile.write(match.group(1))
-                    outfile.write(match.group(2))
-                    outfile.write(match.group(3))
-                line = infile.readline()
+            with open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]),'r') as in_file,\
+                    open(r"C:\Users\{}\Desktop\finalfolder\finalfolderlog.txt".format(user_name), "a") as outfile:
+                line = in_file.readline()
+                while line:
+                    match = re.search(text_match,line)
+                    if match:
+                        reconcile_errors = True
+                        outfile.write(file + "\n")
+                        outfile.write("\t" + line + "\n")
+                    line = in_file.readline()
 
+            if reconcile_errors == False:
+                arcpy.ReconcileVersions_management(file,
+                                                   "ALL_VERSIONS",
+                                                   version_default[0],
+                                                   version_QA[0],
+                                                   "LOCK_ACQUIRED",
+                                                   "ABORT_CONFLICTS",
+                                                   "BY_ATTRIBUTE",
+                                                   "FAVOR_TARGET_VERSION",
+                                                   "POST",
+                                                   "KEEP_VERSION",
+                                                   r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_default[0]))
 
+                with open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_default[0]),
+                          'r') as in_file, \
+                        open(r"C:\Users\{}\Desktop\finalfolder\finalfolderlog.txt".format(user_name), "a") as outfile:
+                    line = in_file.readline()
+                    while line:
+                        match = re.search(text_match, line)
+                        if match:
+                            outfile.write(file + "\n")
+                            outfile.write("\t" + line + "\n")
+                        line = in_file.readline()
 
-
-        ''' Parse logs here. search for failed reconciles/posts, save line of failures, append to new file or dict of db: bad version, stop reconciling of QA to Default
-            there is a race condition here bc the conflict is only identified after the 1st version posts (unless someone has edited QA, naughty!)'''
-
-        arcpy.ReconcileVersions_management(file,
-                                           "ALL_VERSIONS",
-                                           version_default[0],
-                                           version_QA[0],
-                                           "LOCK_ACQUIRED",
-                                           "ABORT_CONFLICTS",
-                                           "BY_ATTRIBUTE",
-                                           "FAVOR_TARGET_VERSION",
-                                           "POST",
-                                           "KEEP_VERSION",
-                                           r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_default[0]))
-
-
-
-        with open(r"C:\Users\{}\Desktop\actual back up folder\reconcile_log.txt".format(user_name),"a") as out_file,\
-             open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name, file, version_default[0]),'r')\
-                     as in_fileDefault:
-            lineDefault = in_fileDefault.readline()
-            while lineDefault:
-                out_file.write(lineDefault)
-                lineDefault = in_fileDefault.readline()
-            out_file.write('\nDone\n')
-
+    except Exception as E:
+        print (E)
+    finally:
+        print ("finally")
 
 def set_workspace():
     user_name = getpass.getuser()
@@ -93,3 +84,4 @@ def set_workspace():
 
 main()
 
+# open(r"C:\Users\{}\Desktop\actual back up folder\reconcile_log.txt".format(user_name), "r") as infile,
