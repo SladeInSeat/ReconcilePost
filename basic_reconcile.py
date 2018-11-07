338c423df506ba767b678b796e546c00c7add5b0
slade <sladenator78@gmail.com>
2018-11-07 13:20:10 -0500

    send mail function improved, ready for testing

--- basic_reconcile.py
+++ basic_reconcile.py
@@ -72,102 +72,102 @@ def main():
         print (conn_files)
         flagged_versions = (versionCheck(static_reconcilelist,version_exclude,conn_files))
         if flagged_versions:
-            print (flagged_versions)
-            print (len(flagged_versions))
-            sendmail("test sendmail function and rec post","JSSawyer@wpb.org", flagged_versions)
+            sendMail("Versions need attention", ["JSSawyer@wpb.org", "JJudge@wpb.org"], flagged_versions)
 
-    #
-    #     for file in conn_files:
-    #         print (file)
-    #         arcpy.AcceptConnections(file, False)
-    #         arcpy.DisconnectUser(file,"ALL")
-    #
-    #         db_string = r"Database Connections\{}".format(file)
-    #         reconcile_errors = False
-    #         versions_children = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string)
-    #                              if version.parentVersionName not in ['sde.DEFAULT',None]
-    #                              and version.name not in version_exclude]
-    #         version_QA = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string)
-    #                       if version.parentVersionName == 'sde.DEFAULT' and version.name[-3:]=='_QA']
-    #         version_default = ['sde.DEFAULT']
-    #
-    #
-    #         if len(versions_children) > 0:
-    #             arcpy.ReconcileVersions_management(file,
-    #                                                "ALL_VERSIONS",
-    #                                                version_QA[0],
-    #                                                versions_children,
-    #                                                "LOCK_ACQUIRED",
-    #                                                "ABORT_CONFLICTS",
-    #                                                "BY_ATTRIBUTE",
-    #                                                "FAVOR_TARGET_VERSION",
-    #                                                "POST",
-    #                                                "KEEP_VERSION",
-    #                                                r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]))
-    #
-    #             with open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]),'r') as in_file:
-    #                 line = in_file.readline()
-    #                 while line:
-    #                     match = re.search(text_match,line)
-    #                     if match:
-    #                         reconcile_errors = True
-    #                         temp_report.write("\n" + file + "\n")
-    #                         temp_report.write("\t" + line + "\n")
-    #                     line = in_file.readline()
-    #         else:
-    #             print ('len of versions_children of {} was < 0'.format(file))
-    #
-    #         if reconcile_errors == False and len(version_QA) > 0:
-    #             arcpy.ReconcileVersions_management(file,
-    #                                                "ALL_VERSIONS",
-    #                                                version_default[0],
-    #                                                version_QA[0],
-    #                                                "LOCK_ACQUIRED",
-    #                                                "ABORT_CONFLICTS",
-    #                                                "BY_ATTRIBUTE",
-    #                                                "FAVOR_TARGET_VERSION",
-    #                                                "POST",
-    #                                                "KEEP_VERSION",
-    #                                                r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_default[0]))
-    #
-    #             with open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_default[0]),
-    #                       'r') as in_file:
-    #                 line = in_file.readline()
-    #                 while line:
-    #                     match = re.search(text_match, line)
-    #                     if match:
-    #                         temp_report.write("\n" + file + "\n")
-    #                         temp_report.write("\t" + line + "\n")
-    #                     line = in_file.readline()
-    #         else:
-    #             temp_report.write(file + " \ndid not reconcile either because of errors (listed above)"
-    #                                      " or No QA version found\n\n")
-    #
-    #     final_report = temp_report.getvalue()
-    #
-    #     if len(final_report) > 0:
-    #         print ("len of final report is > 0, sending email")
-    #         with open(r"C:\Users\{}\Desktop\finalfolder\finalfolderlog.txt".format(user_name), "a") as outfile:
-    #             outfile.write(final_report)
-    #
-    #         today = datetime.datetime.now().strftime("%d-%m-%Y")
-    #         subject = 'Reconcile Post report ' + today
-    #         sendto = "jssawyer@wpb.org"
-    #         sender = 'scriptmonitorwpb@gmail.com'
-    #         sender_pw = "Bibby1997"
-    #         server = 'smtp.gmail.com'
-    #         body_text = "From: {0}\r\nTo: {1}\r\nSubject: {2}\r\n" \
-    #                     "\n{3}".format(sender, sendto, subject, final_report)
-    #
-    #         gmail = smtplib.SMTP(server, 587)
-    #         gmail.starttls()
-    #         gmail.login(sender, sender_pw)
-    #         gmail.sendmail(sender, sendto, body_text)
-    #         gmail.quit()
-    #
-    #
-    #
-    #
+
+        for file in conn_files:
+            print (file)
+            arcpy.AcceptConnections(file, False)
+            arcpy.DisconnectUser(file,"ALL")
+
+            db_string = r"Database Connections\{}".format(file)
+            reconcile_errors = False
+            versions_children = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string)
+                                 if version.parentVersionName not in ['sde.DEFAULT',None]
+                                 and version.name not in version_exclude]
+            version_QA = [version.name.encode('ascii') for version in arcpy.da.ListVersions(db_string)
+                          if version.parentVersionName == 'sde.DEFAULT' and version.name[-3:]=='_QA']
+            version_default = ['sde.DEFAULT']
+
+
+            if len(versions_children) > 0:
+                arcpy.ReconcileVersions_management(file,
+                                                   "ALL_VERSIONS",
+                                                   version_QA[0],
+                                                   versions_children,
+                                                   "LOCK_ACQUIRED",
+                                                   "ABORT_CONFLICTS",
+                                                   "BY_ATTRIBUTE",
+                                                   "FAVOR_TARGET_VERSION",
+                                                   "POST",
+                                                   "KEEP_VERSION",
+                                                   r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]))
+
+                with open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_QA[0]),'r') as in_file:
+                    line = in_file.readline()
+                    while line:
+                        match = re.search(text_match,line)
+                        if match:
+                            reconcile_errors = True
+                            temp_report.write("\n" + file + "\n")
+                            temp_report.write("\t" + line + "\n")
+                        line = in_file.readline()
+            else:
+                print ('len of versions_children of {} was < 0'.format(file))
+
+            if reconcile_errors == False and len(version_QA) > 0:
+                arcpy.ReconcileVersions_management(file,
+                                                   "ALL_VERSIONS",
+                                                   version_default[0],
+                                                   version_QA[0],
+                                                   "LOCK_ACQUIRED",
+                                                   "ABORT_CONFLICTS",
+                                                   "BY_ATTRIBUTE",
+                                                   "FAVOR_TARGET_VERSION",
+                                                   "POST",
+                                                   "KEEP_VERSION",
+                                                   r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_default[0]))
+
+                with open(r"C:\Users\{}\Desktop\reconcile_folder\{}_{}.txt".format(user_name,file,version_default[0]),
+                          'r') as in_file:
+                    line = in_file.readline()
+                    while line:
+                        match = re.search(text_match, line)
+                        if match:
+                            temp_report.write("\n" + file + "\n")
+                            temp_report.write("\t" + line + "\n")
+                        line = in_file.readline()
+            else:
+                temp_report.write(file + " \ndid not reconcile either because of errors (listed above)"
+                                         " or No QA version found\n\n")
+
+        final_report = temp_report.getvalue()
+
+        if len(final_report) > 0:
+            print ("len of final report is > 0, sending email")
+            with open(r"C:\Users\{}\Desktop\finalfolder\finalfolderlog.txt".format(user_name), "a") as outfile:
+                outfile.write(final_report)
+
+            sendMail('Reconcile Post report', ["JSSawyer@wpb.org", "JJudge@wpb.org"], final_report)
+
+            # today = datetime.datetime.now().strftime("%d-%m-%Y")
+            # subject = 'Reconcile Post report ' + today
+            # sendto = "jssawyer@wpb.org"
+            # sender = 'scriptmonitorwpb@gmail.com'
+            # sender_pw = "Bibby1997"
+            # server = 'smtp.gmail.com'
+            # body_text = "From: {0}\r\nTo: {1}\r\nSubject: {2}\r\n" \
+            #             "\n{3}".format(sender, sendto, subject, final_report)
+            #
+            # gmail = smtplib.SMTP(server, 587)
+            # gmail.starttls()
+            # gmail.login(sender, sender_pw)
+            # gmail.sendmail(sender, sendto, body_text)
+            # gmail.quit()
+
+
+
+
     except Exception as E:
         print (E,E.args,sep="\n")
     finally:
@@ -195,7 +195,7 @@ def versionCheck(reconcile, dontreconcile, connectionfiles):
                 new_versions.append(version)
     return new_versions
 
-def sendmail(subject_param,sendto_param, report_param):
+def sendMail(subject_param, sendto_param, report_param):
 
     today = datetime.datetime.now().strftime("%d-%m-%Y")
     subject = "{} {}".format(subject_param,today)
@@ -205,7 +205,8 @@ def sendmail(subject_param,sendto_param, report_param):
     server = 'smtp.gmail.com'
     report = report_param
     body_text = "From: {0}\r\nTo: {1}\r\nSubject: {2}\r\n" \
-                "\n{3}".format(sender, sendto, subject, report)
+                "\nVersions that are older than 30 days and not listed as permanent versions in script\n\t{3}"\
+                .format(sender, sendto, subject, report)
 
     gmail = smtplib.SMTP(server, 587)
     gmail.starttls()
